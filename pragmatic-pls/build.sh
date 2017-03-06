7946d2a8bf130a6ea9e57326fd0ead8f1e8b6405
Alban Mulaki <amulaki@pragmaticlinux.org>
2017-03-06 13:20:36 +0100

    Version 1.2 - FTP Passive, Different Structure,No static ip,NAT

--- pragmatic-pls/build.sh
+++ pragmatic-pls/build.sh
@@ -9,18 +9,28 @@ pacman-optimize
 updatedb
 
 echo "Clear User & Root history,cache.."
-rm -R /root/*
+#rm -R /root/*
 
 
 
 echo "Deleting Logs"
-rm -R /var/log/vsftpd*
-rm -R /var/log/pureftp*
+#rm -R /var/log/vsftpd*
+#rm -R /var/log/pureftp*
 truncate /var/log/pacman.log --size 0
-rm -R /var/log/httpd/*
+#rm -R /var/log/httpd/*
+find /root -exec rm -R '{}' \;
+find /var/log/vsftpd ! -type d -exec rm -R '{}' \;
+find /var/log/pureftp ! -type d -exec rm -R '{}' \;
+find /var/log/httpd ! -type d -exec rm -R '{}' \;
+find /root/ -exec rm -R '{}' \;
 find /var/log/samba ! -type d -exec rm '{}' \;
 find /var/log/journal ! -type d -exec rm '{}' \;
-
+find /srv/ ! -type d -name ".*" -exec rm '{}' \;
+truncate /srv/error_log --size 0
+truncate /srv/http/error_log --size 0
+truncate /srv/http/access_log --size 0
 
 #Build Recompile
 mkinitcpio -p linux
+
+mkdir /root
